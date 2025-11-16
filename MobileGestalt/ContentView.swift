c85e944455937ad4cb1689e912d9996659304cce
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-11-16 13:01:37 +0100

    Initial Commit

---
+++ MobileGestalt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileGestalt
+//
+//  Created by Tim on 16.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileGestalt/MobileGestaltApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileGestaltApp.swift
+//  MobileGestalt
+//
+//  Created by Tim on 16.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MobileGestaltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
